df98cab80ee156de419cafb07752319708164211
aSykoutris <134492329+aSykoutris@users.noreply.github.com>
2024-10-01 17:15:06 +0300

    kasoumis

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,29 +1,29 @@
-import ResumeViewer from "@/components/ResumeViewer";
+import ResumeViewer from '@/components/ResumeViewer';
 export const metadata = {
-  title: "Resume | Emon Singha",
+  title: 'Resume | Kasoumis Giannis',
   description:
-    "Explore the professional resume of Emon Singha, a passionate Frontend Developer with expertise in React and Next.js.",
-  robots: "index, follow",
-  canonical: "https://emonsingha.vercel.app/resume",
+    'Explore the professional resume of Kasoumis Giannis, a passionate Frontend Developer with expertise in React and Next.js.',
+  robots: 'index, follow',
+  canonical: 'https://emonsingha.vercel.app/resume',
   openGraph: {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    name: "Resume",
-    url: "https://emonsingha.vercel.app/resume",
+    '@context': 'https://schema.org',
+    '@type': 'WebPage',
+    name: 'Resume',
+    url: 'https://emonsingha.vercel.app/resume',
     image: {
-      url: "https://emonsingha.vercel.app/img/ogImage.webp",
-      alt: "Emon Singha's Resume",
+      url: 'https://emonsingha.vercel.app/img/ogImage.webp',
+      alt: "Kasoumis Giannis's Resume",
     },
     description:
-      "Explore the professional resume of Emon Singha, a passionate Frontend Developer with expertise in React and Next.js.",
+      'Explore the professional resume of Kasoumis Giannis, a passionate Frontend Developer with expertise in React and Next.js.',
   },
 };
 export default function Resume() {
   return (
-    <div className="grid w-full pt-16 md:pt-5 overflow-auto bg-background place-items-center">
+    <div className='grid w-full pt-16 md:pt-5 overflow-auto bg-background place-items-center'>
       <ResumeViewer
-        file="/resume/Emon-Singha-Resume.pdf"
-        downloadName="Emon-Singha-Resume"
+        file='/resume/Emon-Singha-Resume.pdf'
+        downloadName='Emon-Singha-Resume'
       />
     </div>
   );
--- components/About.jsx
+++ components/About.jsx
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useEffect, useRef, useState } from "react";
-import SpotifyLoading from "./SpotifyLoading";
+} from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useEffect, useRef, useState } from 'react';
+import SpotifyLoading from './SpotifyLoading';
 
-import expertise from "@/public/data/expertise";
-import axios from "axios";
-import { motion } from "framer-motion";
-import Marquee from "react-fast-marquee";
-import { MdDateRange } from "react-icons/md";
-import SvgRenderer from "./SvgRenderer";
-import icons from "./icon/Skill";
-import Link from "next/link";
+import expertise from '@/public/data/expertise';
+import axios from 'axios';
+import { motion } from 'framer-motion';
+import Marquee from 'react-fast-marquee';
+import { MdDateRange } from 'react-icons/md';
+import SvgRenderer from './SvgRenderer';
+import icons from './icon/Skill';
+import Link from 'next/link';
 
 const leftToRight = {
   initial: {
@@ -30,7 +30,7 @@ const leftToRight = {
     opacity: 1,
     transition: {
       duration: 0.3,
-      type: "spring",
+      type: 'spring',
       stiffness: 50,
     },
   },
@@ -45,7 +45,7 @@ const rightToLeft = {
     opacity: 1,
     transition: {
       duration: 0.3,
-      type: "spring",
+      type: 'spring',
       stiffness: 50,
     },
   },
@@ -79,13 +79,13 @@ const About = () => {
       setScreenHeight(window.innerHeight - 72);
     };
 
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", updateDimensions);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', updateDimensions);
 
       updateDimensions();
 
       return () => {
-        window.removeEventListener("resize", updateDimensions);
+        window.removeEventListener('resize', updateDimensions);
       };
     }
   }, []);
@@ -98,29 +98,29 @@ const About = () => {
   }, [divRef, screenHeight]);
 
   return (
-    <section className="flex flex-wrap py-5 container" id="about">
-      <div className="w-full md:w-1/2">
+    <section className='flex flex-wrap py-5 container' id='about'>
+      <div className='w-full md:w-1/2'>
         <motion.h1
           variants={leftToRight}
-          initial="initial"
-          whileInView="animate"
-          className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl"
+          initial='initial'
+          whileInView='animate'
+          className='text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl'
         >
           About Me
         </motion.h1>
         <motion.h2
           variants={leftToRight}
-          initial="initial"
-          whileInView="animate"
-          className="pb-2 mt-8 text-3xl font-semibold tracking-tight transition-colors border-b md:mt-10 scroll-m-20 first:mt-0"
+          initial='initial'
+          whileInView='animate'
+          className='pb-2 mt-8 text-3xl font-semibold tracking-tight transition-colors border-b md:mt-10 scroll-m-20 first:mt-0'
         >
-          Hello, I&apos;m Emon Singha
+          Hello, I&apos;m Kasoumis Giannis
         </motion.h2>
         <motion.p
           variants={leftToRight}
-          initial="initial"
-          whileInView="animate"
-          className="leading-7 [&:not(:first-child)]:mt-6"
+          initial='initial'
+          whileInView='animate'
+          className='leading-7 [&:not(:first-child)]:mt-6'
         >
           I&apos;m a passionate Frontend Developer dedicated to creating
           immersive UI experiences that resonate with users. With a keen eye for
@@ -129,25 +129,25 @@ const About = () => {
         </motion.p>
         <motion.h2
           variants={leftToRight}
-          initial="initial"
-          whileInView="animate"
-          className="pb-2 mt-10 text-3xl font-semibold tracking-tight transition-colors border-b scroll-m-20 first:mt-0"
+          initial='initial'
+          whileInView='animate'
+          className='pb-2 mt-10 text-3xl font-semibold tracking-tight transition-colors border-b scroll-m-20 first:mt-0'
         >
           Experties Area
         </motion.h2>
         {expertise.map((item, index) => (
           <motion.div
             key={index}
-            className="mt-6"
+            className='mt-6'
             variants={leftToRight}
-            initial="initial"
-            whileInView="animate"
+            initial='initial'
+            whileInView='animate'
           >
-            <Card className=" dark:bg-cardBg">
+            <Card className=' dark:bg-cardBg'>
               <CardHeader>
                 <CardTitle>{item.title}</CardTitle>
                 <CardDescription>
-                  <span className="flex flex-wrap gap-3 mt-1 text-lg">
+                  <span className='flex flex-wrap gap-3 mt-1 text-lg'>
                     {item.icons.map((Icon, i) => (
                       <Icon key={i} />
                     ))}
@@ -155,7 +155,7 @@ const About = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <p className="leading-7 [&:not(:first-child)]:mt-6">
+                <p className='leading-7 [&:not(:first-child)]:mt-6'>
                   {item.description}
                 </p>
               </CardContent>
@@ -163,19 +163,19 @@ const About = () => {
           </motion.div>
         ))}
       </div>
-      <div className="w-full mt-8 md:w-1/2 md:mt-0">
+      <div className='w-full mt-8 md:w-1/2 md:mt-0'>
         <div
           ref={divRef}
-          className="sticky flex items-center justify-center overflow-hidden"
+          className='sticky flex items-center justify-center overflow-hidden'
           style={{
-            top: divHeight ? `calc(50% - ${divHeight}px + 32px)` : "80px",
+            top: divHeight ? `calc(50% - ${divHeight}px + 32px)` : '80px',
           }}
         >
           <motion.div
             variants={rightToLeft}
-            initial="initial"
-            whileInView="animate"
-            className="w-full space-y-5 md:w-4/5 "
+            initial='initial'
+            whileInView='animate'
+            className='w-full space-y-5 md:w-4/5 '
           >
             {/* <motion.div
               initial={{ opacity: 0 }}
@@ -200,9 +200,9 @@ const About = () => {
                 </div>
               </div>
             </motion.div> */}
-           
+
             <Marquee pauseOnHover>
-              <ul className="flex gap-5 pr-5 text-5xl flex-nowrap cursor-grabbing">
+              <ul className='flex gap-5 pr-5 text-5xl flex-nowrap cursor-grabbing'>
                 {icons.map((IconSlider, iconIndex) => (
                   <li key={iconIndex}>
                     <IconSlider />
@@ -210,12 +210,12 @@ const About = () => {
                 ))}
               </ul>
             </Marquee>
-            <Tabs defaultValue="experience">
-              <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="experience">Experience</TabsTrigger>
-                <TabsTrigger value="education">Education</TabsTrigger>
+            <Tabs defaultValue='experience'>
+              <TabsList className='grid w-full grid-cols-2'>
+                <TabsTrigger value='experience'>Experience</TabsTrigger>
+                <TabsTrigger value='education'>Education</TabsTrigger>
               </TabsList>
-              <TabsContent value="experience">
+              <TabsContent value='experience'>
                 <Card>
                   <CardHeader>
                     <CardTitle>Experience</CardTitle>
@@ -223,45 +223,45 @@ const About = () => {
                       Gained valuable industry insights.
                     </CardDescription>
                   </CardHeader>
-                  <CardContent className="space-y-6">
-                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-3">
+                  <CardContent className='space-y-6'>
+                    <div className='grid grid-cols-1 xl:grid-cols-2 gap-3'>
                       <Link
-                        href="https://aamrainfotainment.com/"
-                        className="space-y-2 group relative "
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label="AIL"
+                        href='https://aamrainfotainment.com/'
+                        className='space-y-2 group relative '
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        aria-label='AIL'
                       >
-                        <p className="flex items-center gap-1 text-sm">
+                        <p className='flex items-center gap-1 text-sm'>
                           <MdDateRange />3 Sept 2023 - 31 Dec 2023
                         </p>
-                        <h3 className="font-semibold leading-none tracking-wide ">
+                        <h3 className='font-semibold leading-none tracking-wide '>
                           Web Developer - Intern
                         </h3>
                         <p>Aamra Infotainment Ltd.</p>
-                        <div className="absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2"></div>
+                        <div className='absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2'></div>
                       </Link>
                       <Link
-                        href="https://sjinnovation.com/"
-                        className="space-y-2 group relative"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label="SJI"
+                        href='https://sjinnovation.com/'
+                        className='space-y-2 group relative'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        aria-label='SJI'
                       >
-                        <p className="flex items-center gap-1 text-sm">
+                        <p className='flex items-center gap-1 text-sm'>
                           <MdDateRange />3 June 2024 - Present
                         </p>
-                        <h3 className="font-semibold leading-none tracking-wide ">
+                        <h3 className='font-semibold leading-none tracking-wide '>
                           Intern Frontend Developer
                         </h3>
                         <p>SJ Innovation LLC</p>
-                        <div className="absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2"></div>
+                        <div className='absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2'></div>
                       </Link>
                     </div>
                   </CardContent>
                 </Card>
               </TabsContent>
-              <TabsContent value="education">
+              <TabsContent value='education'>
                 <Card>
                   <CardHeader>
                     <CardTitle>Education</CardTitle>
@@ -269,23 +269,23 @@ const About = () => {
                       Academic Background and Qualifications.
                     </CardDescription>
                   </CardHeader>
-                  <CardContent className="space-y-6">
+                  <CardContent className='space-y-6'>
                     <Link
-                      href="https://www.aiub.edu/"
-                      className="space-y-2 group relative"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label="AIUB"
+                      href='https://www.aiub.edu/'
+                      className='space-y-2 group relative'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='AIUB'
                     >
-                      <p className="flex items-center gap-1 text-sm">
+                      <p className='flex items-center gap-1 text-sm'>
                         <MdDateRange />
                         2020 - 2023
                       </p>
-                      <h3 className="font-semibold leading-none tracking-wide">
+                      <h3 className='font-semibold leading-none tracking-wide'>
                         Bachelor of Science in Computer Science & Engineering
                       </h3>
                       <p>American International University-Bangladesh (AIUB)</p>
-                      <div className="absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2"></div>
+                      <div className='absolute w-0 transition-all duration-500 ease-primary group-hover:w-full h-0.5 bg-violet-500 rounded-md -bottom-2'></div>
                     </Link>
                   </CardContent>
                 </Card>
