a7444c7be24a1f322fe2810f496dd3c84ff707f0
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-10-07 20:52:36 +0300

    added notes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,17 +12,17 @@ const Footer = () => {
       </div>
       <footer className='container flex flex-col-reverse md:flex-row items-center justify-center gap-3 py-3 text-sm border-t md:justify-between border-t-border'>
         <div className='flex items-center gap-1 flex-nowrap '>
-          <FaRegCopyright className='w-4 h-4' /> 2024{' '}
-          <span>
+          <FaRegCopyright className='w-4 h-4' /> 2024 Created By
+          <span className=' text-base font-bold underline underline-offset-1 bg-gradient-to-r from-[#8b5cf6] via-[#ffaa40] to-[#8b5cf6] bg-clip-text text-transparent  decoration-foreground transition-all duration-300 hover:decoration-[#8b5cf6]'>
             <Link
-              href='https://www.linkedin.com/in/giannis-kasoumis/'
+              href='https://sykoutris-portfolio-eight.vercel.app/'
               target='_blank'
               rel='noopener noreferrer'
-              aria-label='Linkedin Via Credit'
+              aria-label='Sykoutris Antonis Linkedin Via Credit'
             >
-              Kasoumis Giannis.
-            </Link>
-          </span>{' '}
+              Sykoutris Antonis.
+            </Link>{' '}
+          </span>
           All rights reserved.
         </div>
         <div>
