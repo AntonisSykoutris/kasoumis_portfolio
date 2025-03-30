488439b4f48c7b2ce839ee65fe60d02743362529
Antonis Sykoutris <90038818+AntonisSykoutris@users.noreply.github.com>
2025-03-30 19:38:54 +0300

    Update hero.js

--- public/data/hero.js
+++ public/data/hero.js
@@ -3,4 +3,6 @@ export const HERO_NAME = 'Kasoumis Giannis';
 export const HERO_SUBTITLE = 'Your Friendly';
 
 export const HERO_DESCRIPTION =
-  'Turning data into decisions and ideas into products. As a digital problem-solver, I thrive on building cool stuff that works—because why settle for less?';
+  'Turning data into decisions and ideas into products. As a digital problem-solver, I thrive on building cool stuff that works—because why settle for less??
+  
+  ';
