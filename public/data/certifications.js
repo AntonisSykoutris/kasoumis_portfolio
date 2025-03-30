8a41dc0f1d0625e508b19d02b5445f3ff9999add
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2025-03-30 20:06:04 +0300

    added certs

--- public/data/certifications.js
+++ public/data/certifications.js
@@ -5,11 +5,11 @@ export const CERTIFICATIONS_SUBTITLE = 'Leveling Up: My Stack of Certifications!
 export const certificationsData = [
   {
     id: '1',
-    name: 'Digital Product Management: Modern Fundamentals',
+    name: 'Online Delivery Case Study',
     organization: 'University of Virginia',
     date: 'Completed: 2023',
-    file: 'digital-product-management',
-    image: '/img/one.png',
+    file: 'online-delivery-case',
+    image: '/img/efood.png',
   },
   {
     id: '2',
@@ -25,30 +25,6 @@ export const certificationsData = [
     organization: 'Google',
     date: 'Completed: 2023',
     file: 'advanced-google-analytics',
-    image: '/img/one.png',
-  },
-  {
-    id: '4',
-    name: 'Agile Project Management',
-    organization: 'Google',
-    date: 'Completed: 2023',
-    file: 'agile-project-management',
-    image: '/img/one.png',
-  },
-  {
-    id: '5',
-    name: 'Foundations of Digital Marketing and Ecommerce',
-    organization: 'Google',
-    date: 'Completed: 2023',
-    file: 'foundations-digital-marketing',
-    image: '/img/one.png',
-  },
-  {
-    id: '6',
-    name: 'Foundations of Project Management',
-    organization: 'Google',
-    date: 'Completed: 2023',
-    file: 'foundations-of-project-management',
     image: '/img/one.png',
   },
   // Add more certifications as needed
